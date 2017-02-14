0c8ed863933cfe93638ae269bd0321d2a449e46e
Steve Fisher <dr.s.m.fisher@gmail.com>
2017-02-14 18:00:54 +0000

    Might be ready to go

---
+++ src/main/java/org/icatproject/utils/AddressCheckerException.java
@@ -0,0 +1,10 @@
+package org.icatproject.utils;
+
+@SuppressWarnings("serial")
+public class AddressCheckerException extends Exception {
+
+	public AddressCheckerException(String msg) {
+		super(msg);
+	}
+
+}
